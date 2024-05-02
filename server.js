155ad8c87e055c188d1e250ede66ebdb8ee44c81
Bhashkar Yadav <116943212+bhaskar-5625@users.noreply.github.com>
2024-05-02 12:47:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,18 +15,5 @@ io.on('connection',(socket)=>{
 })
 
 
-// const express = require('express');
-// const socketIO = require('socket.io');
-// const http = require('http');
-// const path = require('path');
-// const app = express();
-// const server = http.createServer(app);
-// const io = socketIO(server);
-// app.use(express.static(path.join(__dirname)));
-// server.listen(3000, () => {
-//     console.log("Server Started");
-// });
-// io.on('connection', (socket) => {
-//     console.log("A user has connected to server");
-// });
+
 
